61829d141b6ab99337d1ccb2de64d94210d8bf4b
apo <apo@opencascade.com>
2005-05-31 09:51:43 +0000

    Introduce a SALOME_IntaractiveObject attribute

--- src/SalomeApp/SalomeApp_DataOwner.cxx
+++ src/SalomeApp/SalomeApp_DataOwner.cxx
@@ -8,24 +8,43 @@
 
 #include <iostream.h>
 
-SalomeApp_DataOwner::SalomeApp_DataOwner( const QString& entry )
-: myEntry( entry )
-{
-  //cout << "---> created DataOwner with entry = " << entry << endl;
-}
-
-SalomeApp_DataOwner::~SalomeApp_DataOwner()
+SalomeApp_DataOwner
+::SalomeApp_DataOwner( const QString& theEntry ): 
+  myEntry( theEntry )
 {
 }
 
-bool SalomeApp_DataOwner::isEqual( const SUIT_DataOwner& obj ) const
+SalomeApp_DataOwner
+::SalomeApp_DataOwner( const Handle(SALOME_InteractiveObject)& theIO ):
+  myEntry(!theIO.IsNull()? theIO->getEntry(): ""),
+  myIO(theIO)
+{
+}
+
+SalomeApp_DataOwner
+::~SalomeApp_DataOwner()
+{
+}
+
+bool
+SalomeApp_DataOwner
+::isEqual( const SUIT_DataOwner& obj ) const
 {
   const SalomeApp_DataOwner* other = dynamic_cast<const SalomeApp_DataOwner*>( &obj );
 
   return other && entry() == other->entry();
 }
 
-QString SalomeApp_DataOwner::entry() const
+QString
+SalomeApp_DataOwner
+::entry() const
 {
   return myEntry;
+}
+
+const Handle(SALOME_InteractiveObject)&
+SalomeApp_DataOwner
+::IO() const
+{
+  return myIO;
 }
--- src/SalomeApp/SalomeApp_DataOwner.h
+++ src/SalomeApp/SalomeApp_DataOwner.h
@@ -2,19 +2,23 @@
 #ifndef SALOMEAPP_DATAOWNER_H
 #define SALOMEAPP_DATAOWNER_H
 
-#include <SUIT_DataOwner.h>
+#include "SUIT_DataOwner.h"
+#include "SALOME_InteractiveObject.hxx"
 
 class SalomeApp_DataOwner : public SUIT_DataOwner
 {
 public:
+    SalomeApp_DataOwner( const Handle(SALOME_InteractiveObject)& theIO );
     SalomeApp_DataOwner( const QString& );
     virtual ~SalomeApp_DataOwner();
 
     virtual bool isEqual( const SUIT_DataOwner& ) const;
-    QString      entry() const;
+    const Handle(SALOME_InteractiveObject)& IO() const;
+    QString entry() const;
 
 private:
     QString  myEntry;
+    Handle(SALOME_InteractiveObject) myIO;
 };
 
 typedef SMART(SalomeApp_DataOwner) SalomeApp_DataOwnerPtr;
