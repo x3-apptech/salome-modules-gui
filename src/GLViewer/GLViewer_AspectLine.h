83b110f855162120b9373fe7dd27ea35c8869059
abd <abd@opencascade.com>
2005-07-01 04:21:19 +0000

    Update comments

--- src/GLViewer/GLViewer_AspectLine.h
+++ src/GLViewer/GLViewer_AspectLine.h
@@ -14,36 +14,84 @@
 #pragma warning( disable:4251 )
 #endif
 
-// Class:   GLViewer_AspectLine
-// Descr:   Substitution of Prs2d_AspectLine for OpenGL
+/*! Class GLViewer_AspectLine
+ *  Substitution of Prs2d_AspectLine for OpenGL
+ *
+ */
 
 class GLVIEWER_API GLViewer_AspectLine 
 {
 public:
+  //! A default constructor
+  /* Line is Non strip
+   * Normal color is white
+   * Highlight color is cyan
+   * Select color is red
+  */
   GLViewer_AspectLine();
-  GLViewer_AspectLine( int, float );
+  //! A conctructor by type and width of line
+  /*!
+   \param theType equals 0 for normal line and 1 for strip line
+   \param theWidth is a width of new line
+  */
+  GLViewer_AspectLine( int theType, float theWidth );
+
+  //! A destructor
   ~GLViewer_AspectLine();
   
+  //! A function for installing the line colors
+  /*!
+   \param nc is normal color
+   \param hc is highlight color
+   \param sc is select color
+  */
   void                  setLineColors( QColor nc = Qt::black,
                                        QColor hc = Qt::cyan,
                                        QColor sc = Qt::red );
-  int                   setLineWidth( const float );
-  int                   setLineType( const int );
-  
-  void                  getLineColors( QColor&, QColor&, QColor& ) const;
+  //! A function for installing the line width
+  int                   setLineWidth( const float theWidth );
+  //! A function for installing the line type
+  /*!
+   \param theType equals 0 for normal line and 1 for strip line
+  */
+  int                   setLineType( const int theType );
+
+  //! A function for getting line colors information
+  /*!
+   \param theNC is normal color
+   \param theHC is highlight color
+   \param theSC is select color
+  */
+  void                  getLineColors( QColor& theNC, QColor& theHC, QColor& theSC ) const;
+  //! A function for getting information about line width
   float                 getLineWidth() const { return myLineWidth; };
+  //! A function for getting information about line type
   int                   getLineType() const { return myLineType; };
   
+  //! A function for coding object to the byte copy
+  /*!
+     A function is used for copy-past technollogy in copy method
+   */
   QByteArray            getByteCopy() const;
   
+  //! A function for decoding object from the byte copy
+  /*!
+     A function is used for copy-past technollogy in past method
+   */
   static GLViewer_AspectLine* fromByteCopy( QByteArray );
   
 protected:
+  //! A normal color
   QColor                myNColor;
+  //! A highlight color
   QColor                myHColor;
+  //! A select color
   QColor                mySColor;
+  //! A line width
   float                 myLineWidth;
-  int                   myLineType;  // 0 - normal, 1 - strip
+  //! A line type
+  /*! equals 0 for normal line and 1 for strip line */
+  int                   myLineType;
 };
 
 #ifdef WNT
