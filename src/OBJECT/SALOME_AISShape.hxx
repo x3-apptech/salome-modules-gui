4aed78babe5c8485df73ad86cfd4cd123c69882a
env <env@opencascade.com>
2005-08-11 03:58:42 +0000

    ENV: Windows porting.

--- src/OBJECT/SALOME_AISShape.hxx
+++ src/OBJECT/SALOME_AISShape.hxx
@@ -77,7 +77,7 @@ Standard_EXPORT SALOME_AISShape(const TopoDS_Shape& shape);
 Standard_EXPORT virtual  Handle_SALOME_InteractiveObject getIO() = 0;
 Standard_EXPORT virtual  Standard_Boolean hasIO() = 0;
 Standard_EXPORT virtual  Standard_CString getName() = 0;
-Standard_EXPORT virtual  void setName(Standard_CString aName) = 0;
+Standard_EXPORT virtual  void setName(const Standard_CString aName) = 0;
 Standard_EXPORT virtual  void highlightSubShapes(const TColStd_IndexedMapOfInteger& aIndexMap, const Standard_Boolean aHighlight ) = 0;
 Standard_EXPORT ~SALOME_AISShape();
 
