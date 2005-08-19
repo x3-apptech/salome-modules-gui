9131b211e3e42fdad87f4789bc12ddbeafa774bc
apo <apo@opencascade.com>
2005-08-19 11:50:16 +0000

    To introduce highlight action

--- src/SVTK/SVTK_Functor.h
+++ src/SVTK/SVTK_Functor.h
@@ -11,38 +11,57 @@
 
 namespace VTK
 {
-  template<class TActor> struct TIsSameEntry
+  template<class TActor> 
+  struct TIsSameEntry
   {
     std::string myEntry;
     TIsSameEntry(const char* theEntry): myEntry(theEntry) {}
     bool operator()(TActor* theActor)
     {
-	    if ( theActor->hasIO() )
+      if ( theActor->hasIO() )
       {
-	      Handle(SALOME_InteractiveObject) anIO = theActor->getIO();
-	      if ( anIO->hasEntry() )
-	        return myEntry == anIO->getEntry();
-	    }
-	    return false;
+	Handle(SALOME_InteractiveObject) anIO = theActor->getIO();
+	if ( anIO->hasEntry() )
+	  return myEntry == anIO->getEntry();
+      }
+      return false;
     }
   };
 
-  template<class TActor> struct TIsSameIObject
+
+  //----------------------------------------------------------------
+  template<class TActor> 
+  struct TIsSameIObject
   {
     Handle(SALOME_InteractiveObject) myIObject;
     TIsSameIObject(const Handle(SALOME_InteractiveObject)& theIObject):
-	    myIObject(theIObject)
+      myIObject(theIObject)
     {}
     bool operator()(TActor* theActor)
     {
-	    if(theActor->hasIO())
+      if(theActor->hasIO())
       {
-	      Handle(SALOME_InteractiveObject) anIO = theActor->getIO();
-	      return myIObject->isSame(anIO);
-	    }
-	    return false;
+	Handle(SALOME_InteractiveObject) anIO = theActor->getIO();
+	return myIObject->isSame(anIO);
+      }
+      return false;
     }
   };
+
+
+  //----------------------------------------------------------------
+  template<class TActor> 
+  struct THighlight
+  {
+    bool myIsHighlight;
+    THighlight(bool theIsHighlight): myIsHighlight( theIsHighlight ) {}
+    void operator()(TActor* theActor) 
+    {
+      if(theActor->GetVisibility() && theActor->GetMapper())
+	theActor->highlight( myIsHighlight );
+    }
+  };
+
 }
 
 
