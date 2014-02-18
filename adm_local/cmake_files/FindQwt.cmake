034a705024b224972c148e1e3834c5ee38df184b
akl <akl@opencascade.com>
2014-02-18 13:37:39 +0400

    Update copyrights 2014.

--- adm_local/cmake_files/FindQwt.cmake
+++ adm_local/cmake_files/FindQwt.cmake
@@ -1,24 +1,9 @@
-# - Find Qwt installation
-# Sets the following variables:
-#   QWT_LIBRARY         - path to the Qwt library
-#   QWT_INCLUDE_DIR     - path to the Qwt headers
-#
-#  The header qwt_plot.h is looked for.
-#  The libraries 
-#      qwt-qt4, qwt
-#  are looked for.
-#
-
-#########################################################################
-# Copyright (C) 2007-2013  CEA/DEN, EDF R&D, OPEN CASCADE
-#
-# Copyright (C) 2003-2007  OPEN CASCADE, EADS/CCR, LIP6, CEA/DEN,
-# CEDRAT, EDF R&D, LEG, PRINCIPIA R&D, BUREAU VERITAS
+# Copyright (C) 2013-2014  CEA/DEN, EDF R&D, OPEN CASCADE
 #
 # This library is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public
 # License as published by the Free Software Foundation; either
-# version 2.1 of the License.
+# version 2.1 of the License, or (at your option) any later version.
 #
 # This library is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -30,6 +15,17 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
 #
 # See http://www.salome-platform.org/ or email : webmaster.salome@opencascade.com
+#
+
+# - Find Qwt installation
+# Sets the following variables:
+#   QWT_LIBRARY         - path to the Qwt library
+#   QWT_INCLUDE_DIR     - path to the Qwt headers
+#
+#  The header qwt_plot.h is looked for.
+#  The libraries 
+#      qwt-qt4, qwt
+#  are looked for.
 #
 
 IF(NOT Qwt_FIND_QUIETLY)
