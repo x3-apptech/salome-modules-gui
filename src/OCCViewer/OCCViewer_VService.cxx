7087d151c50d4c687370eb79eefe9798c6cd2eec
skv <skv@opencascade.com>
2014-07-21 18:00:18 +0400

    OCCT dev version porting (6.7.3)

--- src/OCCViewer/OCCViewer_VService.cxx
+++ src/OCCViewer/OCCViewer_VService.cxx
@@ -26,8 +26,14 @@
 #include <V3d_View.hxx>
 
 #include <Aspect_DisplayConnection.hxx>
+#include <Basics_OCCTVersion.hxx>
+#if OCC_VERSION_LARGE > 0x06070200 // for OCC-6.7.3 and higher version
+#include <OpenGl_GraphicDriver.hxx>
+#else
 #include <Graphic3d.hxx>
 #include <Graphic3d_GraphicDriver.hxx>
+#endif
+
 #ifdef WIN32
 #include <WNT_Window.hxx>
 #else
@@ -61,7 +67,11 @@ Handle(V3d_Viewer) OCCViewer_VService::CreateViewer( const Standard_ExtString na
 						     const Standard_Boolean computedMode,
 						     const Standard_Boolean defaultComputedMode )
 {
+#if OCC_VERSION_LARGE > 0x06070200 // for OCC-6.7.3 and higher version
+  static Handle(OpenGl_GraphicDriver) aGraphicDriver;
+#else
   static Handle(Graphic3d_GraphicDriver) aGraphicDriver;
+#endif
   if (aGraphicDriver.IsNull())
   {
     Handle(Aspect_DisplayConnection) aDisplayConnection;
@@ -70,7 +80,11 @@ Handle(V3d_Viewer) OCCViewer_VService::CreateViewer( const Standard_ExtString na
 #else
     aDisplayConnection = new Aspect_DisplayConnection();
 #endif
+#if OCC_VERSION_LARGE > 0x06070200 // for OCC-6.7.3 and higher version
+    aGraphicDriver = new OpenGl_GraphicDriver(aDisplayConnection);
+#else
     aGraphicDriver = Graphic3d::InitGraphicDriver( aDisplayConnection );
+#endif
   }
 
   return new V3d_Viewer( aGraphicDriver, name, domain, viewSize, viewProjection,
