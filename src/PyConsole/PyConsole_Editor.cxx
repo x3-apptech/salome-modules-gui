83855ea837bef19337db184427e50c70df085042
abn <adrien.bruneton@cea.fr>
2014-09-26 14:51:27 +0200

    Switching console to synchronous mode.

--- src/PyConsole/PyConsole_Editor.cxx
+++ src/PyConsole/PyConsole_Editor.cxx
@@ -169,7 +169,7 @@ PyConsole_Editor::PyConsole_Editor( PyConsole_Interp* theInterp,
   myCmdInHistory( -1 ),
   myEventLoop( 0 ),
   myShowBanner( true ),
-  myIsSync( false ),
+  myIsSync( true ),
   myIsSuppressOutput( false )
 {
   QString fntSet( "" );
