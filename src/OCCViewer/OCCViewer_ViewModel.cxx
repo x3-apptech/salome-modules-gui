x3-apptech/salome-modules-gui

commit b6dff302eaf2433b8e62328a9b00edf1c23e511a

Add used in the future header Prs3d_Drawer.hxx for bug 0022818: EDF 9341 GUI: A compound of one circle is shown as two superimposing circles in the OCC viewer

src/OCCViewer/OCCViewer_ViewModel.cxx
@@ -53,7 +53,11 @@
 #include <QDesktopWidget>
 
 #include <AIS_Axis.hxx>
-#include <AIS_Drawer.hxx>
+#if OCC_VERSION_LARGE > 0x06080000
+  #include <Prs3d_Drawer.hxx>
+#else
+  #include <AIS_Drawer.hxx>
+#endif
 #include <AIS_ListOfInteractive.hxx>
 #include <AIS_ListIteratorOfListOfInteractive.hxx>
 
@@ -121,8 +125,13 @@ OCCViewer_Viewer::OCCViewer_Viewer( bool DisplayTrihedron)
     //myTrihedron->SetColor( Col );
     myTrihedron->SetArrowColor( Col.Name() );
     myTrihedron->SetSize(100);
-    Handle(AIS_Drawer) drawer = myTrihedron->Attributes();
-    if (drawer->HasDatumAspect()) {
+    #if OCC_VERSION_LARGE > 0x06080000
+      Handle(Prs3d_Drawer) drawer = myTrihedron->Attributes();
+      if (drawer->HasOwnDatumAspect()) {
+    #else
+      Handle(AIS_Drawer) drawer = myTrihedron->Attributes();
+      if (drawer->HasDatumAspect()) {
+    #endif
       Handle(Prs3d_DatumAspect) daspect = drawer->DatumAspect();
       daspect->FirstAxisAspect()->SetColor(Quantity_Color(1.0, 0.0, 0.0, Quantity_TOC_RGB));
       daspect->SecondAxisAspect()->SetColor(Quantity_Color(0.0, 1.0, 0.0, Quantity_TOC_RGB));

commit ef7ea81dc99a68c072fe4601bfe523c9572eb3a3

Fix pb of compilation on dev version of OCCT caused by commit b6dff30

src/OCCViewer/OCCViewer_ViewModel.cxx
@@ -125,13 +125,13 @@ OCCViewer_Viewer::OCCViewer_Viewer( bool DisplayTrihedron)
     //myTrihedron->SetColor( Col );
     myTrihedron->SetArrowColor( Col.Name() );
     myTrihedron->SetSize(100);
-    #if OCC_VERSION_LARGE > 0x06080000
+#if OCC_VERSION_LARGE > 0x06080100 // VSR 20/02/2015: temporarily change to larger version until issue OCC25540 is integrated to master; to be reverted later
       Handle(Prs3d_Drawer) drawer = myTrihedron->Attributes();
       if (drawer->HasOwnDatumAspect()) {
-    #else
+#else
       Handle(AIS_Drawer) drawer = myTrihedron->Attributes();
       if (drawer->HasDatumAspect()) {
-    #endif
+#endif
       Handle(Prs3d_DatumAspect) daspect = drawer->DatumAspect();
       daspect->FirstAxisAspect()->SetColor(Quantity_Color(1.0, 0.0, 0.0, Quantity_TOC_RGB));
       daspect->SecondAxisAspect()->SetColor(Quantity_Color(0.0, 1.0, 0.0, Quantity_TOC_RGB));