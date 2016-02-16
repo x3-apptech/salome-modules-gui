f986b18a406575959039956995a57c08ae48fb3b
vsr <vsr@opencascade.com>
2016-02-16 15:58:32 +0300

    On re-creating of the Object browser,  show columns in accordance with preferences.

--- src/SalomeApp/SalomeApp_Application.cxx
+++ src/SalomeApp/SalomeApp_Application.cxx
@@ -1049,6 +1049,12 @@ QWidget* SalomeApp_Application::createWindow( const int flag )
       ob->setResizeOnExpandItem(resizeOnExpandItem);
       ob->setProperty( "shortcut", QKeySequence( "Alt+Shift+O" ) );
 
+      for ( int i = SalomeApp_DataObject::EntryId; i < SalomeApp_DataObject::LastId; i++ )
+      {
+        bool shown = resourceMgr()->booleanValue( "ObjectBrowser", QString( "visibility_column_id_%1" ).arg( i-1 ), true );
+        ob->treeView()->setColumnHidden( i, !shown );
+      }
+
       // temporary commented
       /*
       for ( int i = SalomeApp_DataObject::ValueIdx; i <= SalomeApp_DataObject::RefEntryIdx; i++ )
