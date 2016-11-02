1f938853c8b02533786e39d8e56816538e08e7c1
eap <eap@opencascade.com>
2016-11-02 13:25:58 +0300

    Uninitialized field myStoreMapping

--- src/VTKViewer/VTKViewer_ExtractUnstructuredGrid.cxx
+++ src/VTKViewer/VTKViewer_ExtractUnstructuredGrid.cxx
@@ -51,7 +51,7 @@ vtkStandardNewMacro(VTKViewer_ExtractUnstructuredGrid);
 
 
 VTKViewer_ExtractUnstructuredGrid::VTKViewer_ExtractUnstructuredGrid():
-  myExtractionMode(eCells), myChangeMode(ePassAll)
+  myExtractionMode(eCells), myChangeMode(ePassAll), myStoreMapping( false )
 {}
 
 
