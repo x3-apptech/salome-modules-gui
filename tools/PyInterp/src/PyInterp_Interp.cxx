bee9120a540c2b7e72060ff85c6222a3438239ef
rnv <rnv@opencascade.com>
2017-05-19 15:54:16 +0300

    Python3 porting: stdout and stderr redirects in the embeded python console: small corretion.

--- tools/PyInterp/src/PyInterp_Interp.cxx
+++ tools/PyInterp/src/PyInterp_Interp.cxx
@@ -142,7 +142,7 @@ static PyTypeObject PyStdOut_Type = {
   0,                            /*tp_weaklist*/
   0,                            /*tp_del*/
   0,                            /*tp_version_tag*/
-  0                             /*tp_finalize*/
+  0,                            /*tp_finalize*/
 };
 
 #define PyStdOut_Check(v)  ((v)->ob_type == &PyStdOut_Type)
