3fdf529f8429a544cde2c91ec7daf81511f5e784
vsr <vsr@opencascade.com>
2018-06-13 12:44:47 +0300

    0023564: [EDF] AsterStudy: introduce a feature to show popup notifications - Fix a bug: showNotification() does not return a result

--- src/STD/STD_Application.cxx
+++ src/STD/STD_Application.cxx
@@ -736,6 +736,7 @@ void STD_Application::updateCommandsStatus()
 */
 int STD_Application::showNotification(const QString& message, const QString& title, int timeout)
 {
+  int uid = -1;
   QtxNotify* ntfMgr = notifyMgr();
   if (ntfMgr)
   {
@@ -746,8 +747,9 @@ int STD_Application::showNotification(const QString& message, const QString& tit
       if (aResMgr)
 	delay = aResMgr->integerValue("notification", "timeout", 0) * 1000;
     }
-    ntfMgr->showNotification(message, title, qMax(delay, 0));
+    uid = ntfMgr->showNotification(message, title, qMax(delay, 0));
   }
+  return uid;
 }
 
 /*!
