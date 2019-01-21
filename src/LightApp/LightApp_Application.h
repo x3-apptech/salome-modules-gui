97e015d173f8e7fa8c8e4dfc77ef338d4317e768
rnv <rnv@opencascade.com>
2019-01-21 18:06:12 +0300

    23641: [CEA] FIELDS Window not locked to panel.

--- src/LightApp/LightApp_Application.h
+++ src/LightApp/LightApp_Application.h
@@ -180,7 +180,7 @@ public:
   void                                updateVisibilityState( DataObjectList& theList,
                                                              SUIT_ViewModel* theViewModel );  
 
-  virtual bool                        checkExistingDoc();
+  virtual bool                        checkExistingDoc( bool = true );
 
 #ifndef DISABLE_PYCONSOLE
   PyConsole_Interp*                   getPyInterp();
