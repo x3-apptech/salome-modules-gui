bb7f6ee709da69c3e9ac4985fc5e2e583a83997e
rnv <rnv@opencascade.com>
2021-03-17 17:42:42 +0300

    #20430 [CEA 20428] FIELDS : improvement of simplified visualisations: using SALOME displayer in the non-SALOME viewers (PV viewer for example)

--- src/LightApp/LightApp_Application.cxx
+++ src/LightApp/LightApp_Application.cxx
@@ -5278,7 +5278,7 @@ void LightApp_Application::updateVisibilityState( DataObjectList& theList,
     if ( aDisplayer ) {
       Qtx::VisibilityState anObjState = Qtx::UnpresentableState;
       if ( aDisplayer->canBeDisplayed( obj->entry(), theViewModel->getType() ) ) {
-        if ( aView && aDisplayer->IsDisplayed( obj->entry(), aView ) )
+        if ( aDisplayer->IsDisplayed( obj->entry(), aView ) )
           anObjState = Qtx::ShownState;
         else
           anObjState = Qtx::HiddenState;
